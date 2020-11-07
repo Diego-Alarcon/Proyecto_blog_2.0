e7ce2f94a018efae7ad771c356dfc2a1790ceec8
Diego Alarcon <diegojara277@gmail.com>
2020-11-07 20:25:40 -0300

    casi estamos

--- Pro_Blog/Usuarios/models.py
+++ Pro_Blog/Usuarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -9,3 +10,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+    def save(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
