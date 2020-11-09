781ceed83f8f837b1363f57b4a20d4be7e0df837
Diego Alarcon <diegojara277@gmail.com>
2020-11-09 00:34:08 -0300

    restablecimos la contraseña

--- Pro_Blog/Usuarios/views.py
+++ Pro_Blog/Usuarios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-# Create your views here.
+
 
 def register(request):
     if request.method == 'POST':
@@ -10,35 +10,30 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Usuario Creado Exitosamente!')
+            messages.success(request, f'Cuenta creada con exito!')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'Usuarios/register.html', {'form': form})
-
-
 
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Perfil Actualizado con Exito')
+            messages.success(request, f'Perfil Actualizado')
             return redirect('profile')
-
+    
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-
-
+        
     context = {
-        'u_form' : u_form,
-        'p_form' : p_form
-
+        'u_form': u_form,
+        'p_form': p_form
     }
-
+    
     return render(request, 'Usuarios/profile.html', context)
